fd940de6001e430a424e6fc280cf3c2532d3a29c
kaizuser <thiagochiesa4444@gmail.com>
2022-02-17 11:37:32 +0000

    first commit

---
+++ src/structure/header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Header(){
+	return(
+		<header>
+			<div>
+				<h1>My </h1>
+			</div>
+
+			<nav>
+			</nav>
+
+			<div>
+			</div>
+		</header>
+	)
+}
+
