428937596eecac3e02bcf7c71863d6dd10e8b0bd
kaizuser <thiagochiesa4444@gmail.com>
2022-02-17 13:51:15 +0000

    Header configuration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Header from './structure/header'
+import Main from './structure/main'
 
 function App() {
 	return (
+		<>
 		<Header/>
+		<Main/>
+		</>
 	  );
 }
 
--- src/structure/header.js
+++ src/structure/header.js
@@ -2,15 +2,26 @@ import React from 'react'
 
 export default function Header(){
 	return(
-		<header>
-			<div>
-				<h1>My </h1>
+		<header className='w-full h-24 bg-white flex justify-between items-center hover:bg-gray-200 transition duration-300 ease-in'>
+			<div id='logo-container' className='logo-container flex justify-center items-center'>
+				<svg id='logo' className='m-2 mx-5 transition duration-700 ease-in-out' xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" viewBox="0 0 16 16">
+				  <rect width="4" height="12" rx="1" transform="matrix(1 0 0 -1 6 15)"/>
+				  <path d="M1.5 2a.5.5 0 0 1 0-1v1zm13-1a.5.5 0 0 1 0 1V1zm-13 0h13v1h-13V1z"/>
+				</svg>
 			</div>
 
-			<nav>
+			<nav className='h-20 flex justify-between items-center'> 
+				<h1 className='header m-3 mb-5'>My Tinerary</h1>
+
+				<a className='h-10 px-5 py-2 mt-3 mx-3 h-8 text-xl rounded-xl bg-blue-400 border-b-4 border-white hover:bg-blue-200 hover:border-blue-400 transition duration-300 ease-in flex justify-center items-center' href='../App.js'>Cities</a>
+				<a className='h-10 px-5 py-2 mt-3 mx-3 h-8 text-xl rounded-xl bg-blue-400 border-b-4 border-white hover:bg-blue-200 hover:border-blue-400 transition duration-300 ease-in flex justify-center items-center' href='../App.js'>Home</a>
+
 			</nav>
 
 			<div>
+				<a className=''>
+					<button className='h-10 px-5 py-2 mt-3 mx-1 h-8 text-xl rounded-xl bg-blue-400 border-b-4 border-white hover:bg-blue-200 hover:border-blue-400 transition duration-300 ease-in flex justify-center items-center'>Sign in</button>
+				</a>
 			</div>
 		</header>
 	)
